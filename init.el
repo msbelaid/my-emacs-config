471e0107d0ced77d30a10c8f1678ed949d162d4e
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2020-03-22 15:03:10 +0100

    Initia commit

---
+++ init.el
@@ -0,0 +1,33 @@
+(setq inhibit-startup-message t)
+
+(require 'package)
+;; add MELPA repository 
+(add-to-list 'package-archives
+ 	     '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(load "gnu-elpa-keyring-update")
+
+;; All my initialisation config is in a seperate org file
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smartparens smartparens-config flycheck auto-complete counsel which-key use-package try))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
